8bef1bff4fb12383e0e2fd5cab5fe17b70d66ac3
NITHIN_360 <nithinbalakrishnan569@gmail.com>
2025-11-29 15:43:32 +0530

    feat: add PDF page extraction functionality.

--- client/src/cors/axiousInstance.tsx
+++ client/src/cors/axiousInstance.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", 
-  withCredentials: true,            
+  baseURL: import.meta.env.VITE_API_BASE_URL,  
+  withCredentials: true,
 });
 
 export default api;
+
